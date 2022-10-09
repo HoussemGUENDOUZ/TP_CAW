9845a0a55ac0faefaf178c72335ed81530d0052a
HoussemGUENDOUZ <115425215+HoussemGUENDOUZ@users.noreply.github.com>
2022-10-09 20:28:25 +0100

    Add files via upload

---
+++ TP2/maze.js
@@ -0,0 +1,16 @@
+function lose() {
+  document.getElementById("answer").innerHTML = "you lost!";
+  document.getElementById("answer").style.backgroundColor = "red";
+  const boundary = document.getElementsByClassName("boundary");
+  for (var i = 0; i < 5; i++) {
+    boundary[i].style.backgroundColor = "red";
+  }
+}
+function win() {
+  document.getElementById("answer").innerHTML = "you win!";
+  document.getElementById("answer").style.backgroundColor = "green";
+  const boundary = document.getElementsByClassName("boundary");
+  for (var i = 0; i < 5; i++) {
+    boundary[i].style.backgroundColor = "green";
+  }
+}
