ea797e92f9c49a95c7be2a69b57d20bea65c2288
HoussemGUENDOUZ <115425215+HoussemGUENDOUZ@users.noreply.github.com>
2022-10-15 21:16:46 +0100

    Add files via upload

---
+++ TP2/exo4.js
@@ -0,0 +1,36 @@
+onload = (event) => {
+  var check = Boolean;
+  document.getElementById("end").addEventListener("mouseenter", win);
+  function win() {
+    if (check == Boolean) {
+      document.getElementById("status").innerHTML = "you win!";
+      document.getElementById("status").style.backgroundColor = "green";
+      const boundary = document.getElementsByClassName("boundary");
+      for (var i = 0; i < boundary.length; i++) {
+        boundary[i].style.backgroundColor = "green";
+      }
+      check = true;
+    }
+  }
+  const walls = document.getElementsByClassName("boundary");
+  for (var i = 0; i < walls.length; i++) {
+    walls[i].addEventListener("mouseenter", lose);
+  }
+  function lose() {
+    if (check == Boolean) {
+      document.getElementById("status").innerHTML = "you lost!";
+      document.getElementById("status").style.backgroundColor = "red";
+      const boundary = document.getElementsByClassName("boundary");
+      for (var i = 0; i < boundary.length; i++) {
+        boundary[i].style.backgroundColor = "red";
+      }
+      check = false;
+    }
+  }
+  document.getElementById("start").addEventListener("click", reset);
+  function reset() {
+    if (check == false || check == true) {
+      location.reload(true);
+    }
+  }
+};
