a1a698ec17c64fe2e9d15720b63cf8b418f5bd94
HoussemGUENDOUZ <115425215+HoussemGUENDOUZ@users.noreply.github.com>
2022-11-06 23:48:58 +0100

    Update grep.js

--- TP5/grep.js
+++ TP5/grep.js
@@ -1,1 +1,21 @@
+var fs = require("fs");
 
+fs.readdir(
+  __dirname,
+  function (err, files) {
+    if (err) {
+      return err;
+    }
+    files.forEach((file) => {
+      fs.readFile(file, "utf-8", function (err, data) {
+        if (err) {
+          console.error(err);
+        }
+        var expr = "lorem ipsum";
+        if (data.includes(expr) == true) {
+          console.log(file + "\n");
+        }
+      });
+    });
+  }
+);
